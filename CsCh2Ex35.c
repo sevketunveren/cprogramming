1d8d23d4b8f567c3912c7c8ed8ec18858670b897
sevketunveren <44994836+sevketunveren@users.noreply.github.com>
2018-11-14 17:07:31 +0300

    github

--- CsCh2Ex35.c
+++ CsCh2Ex35.c
@@ -1,12 +1,10 @@
 //https://www.geeksforgeeks.org/write-an-efficient-c-program-to-reverse-bits-of-a-number/
-//CS Ch2Ex35
 #include <stdio.h>
 
-unsigned char reverseBits(unsigned char num)
+char reverseBits(char num)
 {
-	unsigned char NO_OF_BITS = sizeof(num) * 8;
-	unsigned char reverse_num = 0, i, temp;
-
+	char NO_OF_BITS = sizeof(num) * 8;
+	char reverse_num = 0, i, temp;
 	for (i = 0; i < NO_OF_BITS; i++)
 	{
 		printf("\n*****ITERATION%d*****\n",i+1);
@@ -16,14 +14,13 @@ unsigned char reverseBits(unsigned char num)
 			printf("%d OR (1 LEFT SHIFT ((%d - 1) - %d)))\n", reverse_num, NO_OF_BITS, i);
 			reverse_num |= (1 << ((NO_OF_BITS - 1) - i));
 		}
-}
-
+	}
 	return reverse_num;
 }
 
 int main()
 {
-	unsigned char x;
+	char x;
 	printf("Enter a character: ");
 	scanf("%c", &x);
 	printf("REVERSE = %d\n", reverseBits(x));
